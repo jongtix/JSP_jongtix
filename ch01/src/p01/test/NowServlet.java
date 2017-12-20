0b48bf07968d0163ed7106a5c6ee69c9094f48c3
unknown <jong1145@naver.com>
2017-12-20 09:30:06 +0900

    171220

--- ch01/src/p01/test/NowServlet.java
+++ ch01/src/p01/test/NowServlet.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/This")
+@WebServlet("/now")
 public class NowServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;charset = utf-8");
 		PrintWriter out = resp.getWriter();
-		out.print("<html><head><title>현재시간은?</title></head><body>");
-		out.print("현재시간은 ");
+		out.print("<html><head><title>�쁽�옱�떆媛꾩�?</title></head><body>");
+		out.print("�쁽�옱�떆媛꾩� ");
 		out.print(new Date());
-		out.print("입니다.");
+		out.print("�엯�땲�떎.");
 		out.print("</body></html>");
 	}
 }
