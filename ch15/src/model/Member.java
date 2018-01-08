b7429ee305c30ec0059dfd4c5e66fa92fc59f7f3
jongtix <jong1145@naver.com>
2018-01-08 11:39:08 +0900

    180108

---
+++ ch15/src/model/Member.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.util.Date;
+
+// member빈 생성 (dto : DataTransfer Object)
+public class Member {
+	private String id;
+	private String password;
+	private String name;
+	private String zipno;
+	private String address1;
+	private String address2;
+	private String tel;
+	private Date regdate;
+	private char use_flag;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getZipno() {
+		return zipno;
+	}
+
+	public void setZipno(String zipno) {
+		this.zipno = zipno;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public char getUse_flag() {
+		return use_flag;
+	}
+
+	public void setUse_flag(char use_flag) {
+		this.use_flag = use_flag;
+	}
+
+}
